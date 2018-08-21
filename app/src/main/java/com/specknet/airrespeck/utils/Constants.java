4d55435af44573e7768bfe932773683bf7531c7a
andrewbates11 <andrew.bates11@gmail.com>
2018-08-21 21:28:47 +0100

    update upload URL for testing

--- app/src/main/java/com/specknet/airrespeck/utils/Constants.java
+++ app/src/main/java/com/specknet/airrespeck/utils/Constants.java
@@ -38,7 +38,7 @@ public class Constants {
     /**
      * UPLOAD SERVICES
      */
-    public static final String UPLOAD_SERVER_URL = "https://specknet-pyramid-test.appspot.com/";
+    public static final String UPLOAD_SERVER_URL = "https://rat-dot-specknet-pyramid-test.appspot.com/";
     public static final String UPLOAD_SERVER_PATH = "uploadAirRespeck";
 
 
